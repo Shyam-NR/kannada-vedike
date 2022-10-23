796ff6dac94567a120da2235fade58c6b1505f16
Shyam-NR <nrshyamsundar@gmail.com>
2022-10-23 12:28:08 +0530

    add baseUrl

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     
-    <BrowserRouter>
+    <BrowserRouter basename='/kannada-vedike'>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
